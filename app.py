84e5010d86a7a5341b49caa22f98730fcb17dada
officialfareedahmad <officialfareedahmad@gmail.com>
2024-12-16 22:30:22 +0500

    developed app for prediction model testing

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, request, render_template
+import pandas as pd
+import numpy as np
+import pickle   
+
+
+model = pickle.load(open('breast_cancer.pkl', 'rb'))
+
+
+#flask app
+
+app = Flask(__name__)
+
+
+@app.route('/')
+
+def index():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    features = request.form['feature']
+    features_list= features.split(',')
+    np_features = np.asarray(features_list,dtype=np.float32)
+    pred = model.predict(np_features.reshape(1,-1))
+
+    output = ["cancerous" if pred[0] == 1 else "not cancerous"]
+
+    return render_template('index.html' , message = output)
+
+
+
+#python main
+
+if __name__ == "__main__":
+    app.run(debug=True)
